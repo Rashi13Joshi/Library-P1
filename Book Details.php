e806e10d17b481a5d898f29f5cd6c2cc2fd4ca1f
Rashi13Joshi <76475809+Rashi13Joshi@users.noreply.github.com>
2021-03-06 23:50:55 -0800

    Add files via upload

---
+++ Book Details.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+<title>Details</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Aclonica&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+<style>
+h1{
+  font-size: 4vw;
+  font-family: 'Aclonica', sans-serif;
+  padding-top: 15px;
+}
+
+div {
+  height: 100%;
+}
+
+.container {
+  display: table;
+  width: 100%;
+}
+
+.left-half {
+  background-color: #fff;
+  position: absolute;
+  left: 0px;
+  width: 60%;
+  color:grey;
+  height:auto;
+}
+
+.right-half {
+  background-color: #DC143C;
+  position: absolute;
+  right: 0px;
+  width: 40%;
+  color:#fff;
+  height:auto;
+}
+
+h4{
+  font-size: 1.5vw;
+  padding-top: 12px;
+  float:right;
+  color: #fff;
+}
+@media screen and (min-width:50rem){
+  img{
+    height:500px;
+    width:350px;
+    float:right;
+    border-bottom: 2px solid transparent;
+  }
+}
+
+img{
+  float:right;
+  border-left: 3px solid transparent;
+  border-bottom: 3px solid transparent;
+}
+p{
+  font-size: 1.2vw;
+  width:28em;
+}
+#padding{
+  padding-top: 30px;
+}
+
+.button{
+    color:#fff;
+    font-size: 1.2vw;
+    border: 1px solid #DC143C;
+    border-radius: 5px;
+    text-align: center;
+    width: 20%;
+    font-family: 'Montserrat Alternates', sans-serif;
+    background-color: #DC143C;
+
+}
+
+</style>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-8 left-half">
+      <h1>Veronika decides to die</h1>
+       <img src="https://images-na.ssl-images-amazon.com/images/I/81UqvCm-RnL.jpg" height="150" width="100">
+
+            <p><b>Description</b><br>'Veronika Decides to Die' is a beautiful tale of hope and happiness. The story
+             revolves around Veronika, a young Slovenian woman who has everything that a woman in the prime of
+             her life would want. She has a good job, a string of boyfriends and a family that loves her. However,
+              she is not happy with her life and decides to committ suicide. After taking sleeping pills, she awaits
+              her death which does not arrive. She wakes up in a hospital where she is told that the pills have
+              shortened her life span and that she has only a few days to live. Within the walls of a mental hospital,
+              Veronika falls in love with a person suffering from schizophrenia. The head psychiatrist at the hospital
+              is trying to perform an experiment, which concerns Veronika in some manner.<br> How and why is this happening?
+              That is what the story is about.<br>Veronika Decides to Die is loosely based on the author’s experiences
+              in mental institutions. The book has been translated in 46 languages worldwide and has been adapted for
+              theatre shows multiple times. The book has also received a lot of praise from the critics.<br> The Daily
+              Express has termed the book as a poetic piece of literature that can give the readers hope and happiness,
+              whereas the Independent has termed the book as a publishing phenomenon. Veronika Decides to Die has also
+              been adapted into a major motion picture by Das Films with Muse Productions.
+              <a href="http://bookophile.weebly.com/uploads/6/4/0/8/6408830/veronika_decides_to_die.pdf">
+                Try book</a></p>
+
+        <a href="Edit book.php"><h3 class="button">Edit book details</h3></a>
+        <a href="Delete book.php"><h3 class="button">Remove book</h3></a>
+    </div>
+    <div class="col right-half">
+       <a href="Library.php"><h4><i class="fas fa fa-arrow-left">&nbsp;</i>Back</h4></a>
+       <p id="padding"><b>About the author</b><br>The Brazilian author PAULO COELHO was born in 1947 in the city of Rio de Janeiro. Before dedicating his life
+         completely to literature, he worked as theatre director and actor, lyricist and journalist. In 1986, PAULO COELHO
+         did the pilgrimage to Saint James of Compostella, an experience later to be documented in his book The Pilgrimage.
+         In the following year, COELHO published The Alchemist. Slow initial sales convinced his first publisher to drop the
+         novel, but it went on to become one of the best selling Brazilian books of all time. Other titles include Brida (1990),
+         The Valkyries (1992), By the river Piedra I sat Down and Wept (1994), the collection of his best columns published in
+         the Brazilian newspaper Folha de São Paulo entitle Maktub (1994), the compilation of texts Phrases (1995), The Fifth
+         Mountain (1996), Manual of a Warrior of Light (1997), Veronika decides to die (1998), The Devil and Miss Prym (2000),
+         the compilation of traditional tales in Stories for parents, children and grandchildren (2001), Eleven Minutes (2003),
+         The Zahir (2005), The Witch of Portobello (2006) and Winner Stands Alone (to be released in 2009). During the months of
+         March, April, May and June 2006, Paulo Coelho traveled to celebrate the 20th anniversary of his pilgrimage to Saint James
+         of Compostella in 1986. He also held surprise book signings - announced one day in advance - in some cities along the way,
+         to have a chance to meet his readers. In ninety days of pilgrimage the author traveled around the globe and took the famous
+         Transiberrian train that took him to Vladivostok. During this experience Paulo Coelho launched his blog Walking the Path -
+         The Pilgrimage in order to share with his readers his impressions. Since this first blog Paulo Coelho has expanded his presence
+         in the internet with his daily blogs in Wordpress, Myspace & Facebook. He is equally present in media sharing sites such as Youtube and
+         Flickr, offering on a regular basis not only texts but also videos and pictures to his readers.</p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
