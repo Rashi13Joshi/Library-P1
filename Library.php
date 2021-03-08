802705bff9c8f21987caf0d09b68429c3a78ec61
Rashi13Joshi <76475809+Rashi13Joshi@users.noreply.github.com>
2021-03-08 11:58:54 -0800

    Update Library.php

--- Library.php
+++ Library.php
@@ -1,3 +1,4 @@
+
 <html>
 <head>
    <title>Library</title>
@@ -15,7 +16,7 @@
     }
     h4{
       color:#fff;
-      font-size: 1vw;
+      font-size: 1.3vw;
       border: 2px solid #DC143C;
       border-radius: 5px;
       text-align: center;
@@ -34,9 +35,7 @@ p{
   font-size: 75%;
   font-family: 'Montserrat Alternates', sans-serif;
 }
-img{
-  border-radius: 15px;
-}
+
 /*.field{
   border-radius: 15px;
   background-color: grey;
@@ -48,18 +47,53 @@ body {
   background-image: url("imageedit_2_6915477300.png");
   background-size: cover;
 }
+
+img{
+  border-top: 4px solid #fff;
+  border-right: 4px solid #fff;
+  border-left: 4px solid #fff;
+  border-bottom: 4px solid #fff;
+  background-color: #fff;
+  border-radius: 10px;
+}
+p{
+  border-top: 4px solid #fff;
+  border-right: 4px solid #fff;
+  border-left: 4px solid #fff;
+  border-bottom: 4px solid #fff;
+  background-color: #fff;
+  border-radius: 10px;
+
+}
    </style>
 </head>
 <body>
   <h1>Be awesome...be a book nut!!</h1>
-  <h4>Add More ! !</h4>
+  <a href="Add Books.php"><h4>Add More</h4></a>
 <br>
 <!------------------------------------------------------------------------------------>
 <div class="container">
       <div class="row ">
-        <div class="col">
+        <?php
+        $link = mysqli_connect("localhost", "root", "", "library",);
+        $db=mysqli_select_db($link,'dbsetup.php');
+        $query="SELECT * FROM books";
+        $query_run=mysqli_query($link,$query);
+
+        if($query_run)
+        {
+          foreach($query_run as $row)
+          {
+            ?><div class="col">
+              <img src="<?php echo $row['image_url'];?>" height="120" width="120">
+              <p><b><?php echo $row['name'];?> <br><i><?php echo $row['authors'];?></i><br><a href="Book Details.php"target="_blank">Know more</a></p></b>
+            </div><?php
+          }
+        }
+        ?>
+      <!--  <div class="col">
           <img src="https://images-na.ssl-images-amazon.com/images/I/81UqvCm-RnL.jpg" height="120" width="120">
-          <p><b>Veronika decides to die<br><i>Paulo Coelho</i><br><a href="">Know more</a></p></b>
+          <p><b>Veronika decides to die<br><i>Paulo Coelho</i><br><a href="Book Details.php"target="_blank">Know more</a></p></b>
         </div>
         <div class="col">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gt2QslXEQeu-2ulFhYxIzYN8mDcEFzNGIg&usqp=CAU" height="120" width="120">
@@ -106,9 +140,12 @@ body {
       <div class="col">
         <img src="https://i.pinimg.com/originals/5c/11/ea/5c11eabef9ba218d03ee8f9d444618e2.jpg" height="120" width="120">
         <p><b>Optimizing C++<br><i>Steve Heller</i><br><a href="">Know more</a></p></b>
-      </div>
+      </div>-->
   </div>
 </div>
 </body>
 
 </html>
+
+
+<!----http://localhost/phpmyadmin/sql.php?db=library&table=books&pos=0-->
