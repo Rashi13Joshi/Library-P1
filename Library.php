c3c38e6c71add6b4c30024fa15ca19aac1591b16
Rashi13Joshi <76475809+Rashi13Joshi@users.noreply.github.com>
2021-03-08 12:23:53 -0800

    Update Library.php

--- Library.php
+++ Library.php
@@ -84,63 +84,14 @@ p{
         {
           foreach($query_run as $row)
           {
-            ?><div class="col">
+            ?><div class="col-2">
               <img src="<?php echo $row['image_url'];?>" height="120" width="120">
               <p><b><?php echo $row['name'];?> <br><i><?php echo $row['authors'];?></i><br><a href="Book Details.php"target="_blank">Know more</a></p></b>
             </div><?php
           }
         }
         ?>
-      <!--  <div class="col">
-          <img src="https://images-na.ssl-images-amazon.com/images/I/81UqvCm-RnL.jpg" height="120" width="120">
-          <p><b>Veronika decides to die<br><i>Paulo Coelho</i><br><a href="Book Details.php"target="_blank">Know more</a></p></b>
-        </div>
-        <div class="col">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gt2QslXEQeu-2ulFhYxIzYN8mDcEFzNGIg&usqp=CAU" height="120" width="120">
-          <p><b>An Orphan's War<br><i>Molly Green</i><br><a href="">Know more</a></p></b>
-        </div>
-        <div class="col">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR46Ua7s1R0ti_i2ZuhlRGfjBH-uLX-0lY9uw&usqp=CAU" height="120" width="120">
-          <p><b>The Book of Lost Names<br><i>Kristin Harmel</i><br><a href="">Know more</a></p></b>
-        </div>
-        <div class="col">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkYhEP2k02h40P03HLd8dVIna0FUVQc6pODA&usqp=CAU" height="120" width="120">
-          <p><b>To be Taught if fortunate<br><i>Becky Chambers</i><br><a href="">Know more</a></p></b>
-        </div>
-        <div class="col">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC-CTFl2X67erg4dmzNG_iiYRq_Hc1ulx5lw&usqp=CAU" height="120" width="120">
-          <p><b>Yellow Wife<br><i>Sadeh Johnson</i><br><a href="">Know more</a></p></b>
-        </div>
-        <div class="col">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4rYk70Li4gE6f5ksXdbchWmKL_pe50rnd-g&usqp=CAU" height="120" width="120">
-          <p><b>Cage Of Souls<br><i>Adrian Tchaikovsky</i><br><a href="">Know more</a></p></b>
-        </div>
-      </div>
-  <div class="row">
-      <div class="col">
-        <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1534693322l/38926328.jpg" height="120" width="120">
-        <p><b>Five Feet Apart<br><i>Rachael Lipincott,Mikki Daughtry,Tobias Laconis</i><br><a href="">Know more</a></p></b>
-      </div>
-      <div class="col">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLBPGkpnP0IFR8BeZKYTI8T57tZpp3f4Ub1Q&usqp=CAU" height="120" width="120">
-        <p><b>A brief history of time<br><i>Stephen Hawking</i><br><a href="">Know more</a></p></b>
-      </div>
-      <div class="col">
-        <img src="http://aiprobook.com/img/dlfp-cover.png" height="120" width="120">
-        <p><b>Deep Learning For programmers<br><i>Dragan Juric</i><br><a href="">Know more</a></p></b>
-      </div>
-      <div class="col">
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51XrviXU8eL._SX329_BO1,204,203,200_.jpg" height="120" width="120">
-        <p><b>A Handbook Of Soft Skills<br><i>Krishnaveer Abhishek Challa,Santoshkumar karimilli</i><br><a href="">Know more</a></p></b>
-      </div>
-      <div class="col">
-        <img src="https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0999685902&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=anysoftwaretools-20&language=en_US" height="120" width="120">
-        <p><b>The Self Taught Programmer<br><i>Cory Althoff</i><br><a href="">Know more</a></p></b>
-      </div>
-      <div class="col">
-        <img src="https://i.pinimg.com/originals/5c/11/ea/5c11eabef9ba218d03ee8f9d444618e2.jpg" height="120" width="120">
-        <p><b>Optimizing C++<br><i>Steve Heller</i><br><a href="">Know more</a></p></b>
-      </div>-->
+
   </div>
 </div>
 </body>
