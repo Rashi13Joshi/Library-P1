247edef64526412effdb876375d4f63d88fb6f09
Rashi13Joshi <76475809+Rashi13Joshi@users.noreply.github.com>
2021-03-10 01:39:09 -0800

    Update Library.php

--- Library.php
+++ Library.php
@@ -1,27 +1,34 @@
 
 <html>
-<head>
+ <head>
    <title>Library</title>
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="preconnect" href="https://fonts.gstatic.com">
    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates&family=Reggae+One&display=swap" rel="stylesheet">
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
    <style>
+
+   body {
+     background-image: url("imageedit_2_6915477300.png");
+     background-size: cover;
+   }
+
     h1{
       text-align: center;
       font-family: 'Reggae One', cursive;
       color:#DC143C;
-      font-size: 3vw;
+      font-size: 4vw;
       padding-top: 20px;
     }
+
     h4{
       color:#fff;
-      font-size: 1.3vw;
-      border: 2px solid #DC143C;
+      font-size: 2vw;
+      border: 1px solid #DC143C;
       border-radius: 5px;
       text-align: center;
       margin: auto;
-      width: 10%;
+      width: 20%;
       padding: 8px;
       font-family: 'Montserrat Alternates', sans-serif;
       background-color: #DC143C;
@@ -31,10 +38,6 @@
       max-width: 50px;
       height: auto;
     }*/
-p{
-  font-size: 75%;
-  font-family: 'Montserrat Alternates', sans-serif;
-}
 
 /*.field{
   border-radius: 15px;
@@ -43,57 +46,66 @@ p{
   height:200px;
 }*/
 
-body {
-  background-image: url("imageedit_2_6915477300.png");
-  background-size: cover;
-}
 
-img{
-  border-top: 4px solid #fff;
-  border-right: 4px solid #fff;
-  border-left: 4px solid #fff;
-  border-bottom: 4px solid #fff;
-  background-color: #fff;
-  border-radius: 10px;
-}
-p{
-  border-top: 4px solid #fff;
-  border-right: 4px solid #fff;
-  border-left: 4px solid #fff;
-  border-bottom: 4px solid #fff;
-  background-color: #fff;
-  border-radius: 10px;
 
-}
-   </style>
+    img{
+      border-top: 4px solid #fff;
+      border-right: 4px solid #fff;
+      border-left: 4px solid #fff;
+      border-bottom: 4px solid #fff;
+      background-color: #fff;
+      border-radius: 10px;
+    }
+
+    p{
+      border-top: 4px solid #fff;
+      border-right: 4px solid #fff;
+      border-left: 4px solid #fff;
+      border-bottom: 4px solid #fff;
+      background-color: #fff;
+      border-radius: 10px;
+      width:180px;
+      font-size: 75%;
+      font-family: 'Montserrat Alternates', sans-serif;
+      margin-bottom: 35px;
+    }
+
+    @media screen and (max-width:50rem){
+      p{
+        width:140px;
+      }
+
+    }
+  </style>
 </head>
 <body>
   <h1>Be awesome...be a book nut!!</h1>
   <a href="Add Books.php"><h4>Add More</h4></a>
-<br>
+  <br>
 <!------------------------------------------------------------------------------------>
-<div class="container">
-      <div class="row ">
-        <?php
-        $link = mysqli_connect("localhost", "root", "", "library",);
-        $db=mysqli_select_db($link,'dbsetup.php');
-        $query="SELECT * FROM books";
-        $query_run=mysqli_query($link,$query);
+  <div class="container">
+        <div class="row ">
+          <?php
+          $link = mysqli_connect("localhost", "root", "", "library",);
+          $db=mysqli_select_db($link,'dbsetup.php');
+          $query="SELECT * FROM books";
+          $query_run=mysqli_query($link,$query);
 
-        if($query_run)
-        {
-          foreach($query_run as $row)
+          if($query_run)
           {
-            ?><div class="col-2">
-              <img src="<?php echo $row['image_url'];?>" height="120" width="120">
-              <p><b><?php echo $row['name'];?> <br><i><?php echo $row['authors'];?></i><br><a href="Book Details.php"target="_blank">Know more</a></p></b>
-            </div><?php
-          }
-        }
-        ?>
+            foreach($query_run as $row)
+            {
+              ?><div class="col">
+                <img src="<?php echo $row['image_url'];?>" height="150" width="150">
+                <p><b><?php echo $row['name'];?> <br><i><?php echo $row['authors'];?></i><br><a href="Book Details.php?id=<?php echo $row['id']?>">Know more</a></p></b>
+              </div><?php
+            }
 
+          }
+          ?>
+
+      </div>
   </div>
-</div>
 </body>
 
 </html>
